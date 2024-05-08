74ce87d08c0d1ace4e5e9df1f40b1682135f0239
Elisa Semedo <elisasemedo07@gmail.com>
2024-05-09 01:18:20 +0200

    création des controllers de  mise à jour du statut et de la priorité

--- src/routes/deiRoutes.ts
+++ src/routes/deiRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 
-import { getAllDEI, updateStatusDEI } from '../controllers/DeiController'
+import { getAllDEI, updatePriority, updateStatusDEI, updateStatusSacha } from '../controllers/DeiController'
 
 const router = Router()
 
 router.get('/', getAllDEI)
-router.patch('/', updateStatusDEI)
+router.patch('/:id/status', updateStatusDEI)
+router.patch('/:id/sachaStatus', updateStatusSacha)
+router.patch('/:id/priority', updatePriority)
 
 export default router
