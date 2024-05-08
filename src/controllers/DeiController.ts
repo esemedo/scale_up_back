12a2d65970564d3c2e0ebbfabc21adf59731bbbd
Elisa Semedo <elisasemedo07@gmail.com>
2024-05-09 01:18:20 +0200

    création des controllers de  mise à jour du statut et de la priorité

--- src/controllers/DeiController.ts
+++ src/controllers/DeiController.ts
@@ -5,7 +5,21 @@ const prisma = new PrismaClient();
 
 const getAllDEI = async (req: Request, res: Response) => {
     try {
-        const deiEntries = await prisma.dei.findMany();
+      const {priority } = req.query
+      let whereClause = {}
+      const parsedPriority = parseInt(String(priority))
+      if (priority && !isNaN(parsedPriority)){
+        whereClause = {priority: parsedPriority}}
+        const deiEntries = await prisma.dei.findMany({
+          where:whereClause,
+          orderBy: [
+            
+            {
+              id: 'desc',
+            },
+          ],
+        });
+        
         res.json(deiEntries);
     } catch (error) {
         res.status(500).json({ error: 'Could not retrieve DEI entries' });
@@ -14,19 +28,67 @@ const getAllDEI = async (req: Request, res: Response) => {
 
 const updateStatusDEI = async (req: Request, res: Response) => {
     try {
-        const {id} = req.body
-       await prisma.dei.update({
+        const {id} = req.params
+        const {status} = req.body
+
+       const updated = await prisma.dei.update({
             where: {
-              id,
+              id: parseInt(id),
             },
             data: {
-              status: true,
+              status: Boolean(status),
             },
           })
+          console.log(updated);
+          
         res.json({message: "Task updated !"});
 
     } catch (error) {
         res.status(500).json({ error: 'Could not update DEI status ' });
     }
 };
-export { getAllDEI, updateStatusDEI };
+
+const updateStatusSacha = async (req: Request, res: Response) => {
+  try {
+      const {id} = req.params
+      const { sachaStatus} = req.body
+
+     const updated = await prisma.dei.update({
+          where: {
+            id: parseInt(id),
+          },
+          data: {
+            sashaStatus: sachaStatus
+          },
+        })
+        console.log(updated);
+        
+      res.json({message: "SACHA Status updated !"});
+
+  } catch (error) {
+      res.status(500).json({ error: 'Could not update SACHA status ' });
+  }
+};
+
+const updatePriority = async (req: Request, res: Response) => {
+  try {
+      const {id} = req.params
+      const { priority} = req.body
+
+     const updated = await prisma.dei.update({
+          where: {
+            id: parseInt(id),
+          },
+          data: {
+            priority: priority
+          },
+        })
+        console.log(updated);
+        
+      res.json({message: "Priotity updated !"});
+
+  } catch (error) {
+      res.status(500).json({ error: 'Could not update Priority ' });
+  }
+};
+export { getAllDEI, updateStatusDEI, updateStatusSacha, updatePriority };
