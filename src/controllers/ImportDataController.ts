ca1f23e3f9749e6070a19ba424090bc1f5e474bb
Daadaa Nabil <103863031+RyzKum@users.noreply.github.com>
2024-05-14 10:04:30 +0200

    fixing import data with file

--- src/controllers/ImportDataController.ts
+++ src/controllers/ImportDataController.ts
@@ -1,48 +1,42 @@
-import Papa from 'papaparse'
-import readXlsxFile from 'read-excel-file/node'
 import { prisma } from '../index'
 
 export const importData = async (req, res) => {
-    let file = req.file
-    let categoryId = req.body.categoryId 
-    let data
+    let data = req.body.data;
+    let categoryId = req.body.categoryId;
 
-    if (file.mimetype === 'text/csv') {
-        data = await new Promise((resolve) => {
-            Papa.parse(file.path, {
-                header: true,
-                skipEmptyLines: true,
-                complete: (results) => {
-                    resolve(results.data)
-                }
-            })
-        })
-    } else if (file.mimetype === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
-        data = await readXlsxFile(file.path)
-    } else {
-        return res.status(400).json({ error: 'Invalid file type. Only CSV and XLSX files are allowed.' })
+    if (!data) {
+        res.status(400).json({ error: 'Request body is missing or malformed' });
+        return;
+    }
+    
+    // Convertir la chaîne JSON en tableau
+    try {
+        data = JSON.parse(data);
+    } catch (error) {
+        res.status(400).json({ error: 'Invalid JSON format' });
+        return;
     }
 
     // Importe les données dans la base de données
     for (let row of data) {
-        if (req.path.includes('subjects')) {
-            await prisma.subject.create({
-                data: {
-                    name: row.name,
-                    level: row.level,
-                    categoryId: parseInt(categoryId)
-                }
-            })
-        } else if (req.path.includes('promotions')) {
-            await prisma.promotion.create({
-                data: {
-                    startSchoolYear: parseInt(row.startSchoolYear),
-                    endSchoolYear: parseInt(row.endSchoolYear),
-                    managerId: parseInt(row.managerId)
-                }
-            })
-        }
+      if (req.path.includes('subjects')) {
+        await prisma.subject.create({
+          data: {
+            name: row[0], 
+            level: row[1], 
+            categoryId: parseInt(categoryId)
+          }
+        });
+      } else if (req.path.includes('promotions')) {
+        await prisma.promotion.create({
+          data: {
+            startSchoolYear: parseInt(row.startSchoolYear),
+            endSchoolYear: parseInt(row.endSchoolYear),
+            managerId: parseInt(row.managerId)
+          }
+        })
+      }
     }
-
+  
     res.status(200).json({ message: 'Data imported successfully' })
 }
--- src/routes/ImportDataRoutes.ts
+++ src/routes/ImportDataRoutes.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express'
-import upload from '../middlewares/uploadData'
 import { importData } from '../controllers/ImportDataController'
 
 const router = Router()
 
-router.post('/subjects', upload.single('file'), importData)
-router.post('/promotions', upload.single('file'), importData)
+router.post('/subjects', importData)
+router.post('/promotions', importData)
 
 export default router
