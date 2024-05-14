f3fcafa2d7144b530cd98eb03c921e828790a137
justoverfr <jeanjuste.lefebvre@gmail.com>
2024-05-14 16:16:41 +0200

    chore: typing

--- src/middlewares/handleValidator.ts
+++ src/middlewares/handleValidator.ts
@@ -1,6 +1,7 @@
+import { NextFunction, Request, Response} from "express";
 import { validationResult } from "express-validator";
 
-export const handleValidationErrors = (req, res, next) => {
+export const handleValidationErrors = (req: Request, res: Response, next: NextFunction) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
