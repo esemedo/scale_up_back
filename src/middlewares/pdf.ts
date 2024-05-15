14a7ae621892c77adfe15092fbbd0339cd0e6165
Sambachpx <113524274+Sambachpx@users.noreply.github.com>
2024-05-15 13:41:54 +0200

    fix: Improve PDF generation functionality and error handling

--- src/middlewares/pdf.ts
+++ src/middlewares/pdf.ts
@@ -6,6 +6,8 @@ export const pdfMiddleware = (
   next: NextFunction
 ) => {
   const data = req.body;
+  console.log("données reçues pour la génération du PDF :", data);
+
   const missingFields = [
     "hourlyPrice",
     "hoursVolume",
@@ -14,10 +16,12 @@ export const pdfMiddleware = (
   ].filter((field) => !data[field]);
 
   if (missingFields.length > 0) {
+    console.log("champs manquants pour la génération du PDF :", missingFields);
     res
       .status(400)
       .send(`missing data for generation: ${missingFields.join(", ")}`);
   } else {
+    console.log("données valides pour la génération du PDF");
     next();
   }
 };
