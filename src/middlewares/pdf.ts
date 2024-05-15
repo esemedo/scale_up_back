04d8d638b19b9cf95b1a9abfd5b9e8d017490d88
Sambachpx <113524274+Sambachpx@users.noreply.github.com>
2024-05-15 09:17:47 +0200

    feat: Add PDF generation functionality

---
+++ src/middlewares/pdf.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+
+export const pdfMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const data = req.body;
+  const missingFields = [
+    "hourlyPrice",
+    "hoursVolume",
+    "startDate",
+    "endDate",
+  ].filter((field) => !data[field]);
+
+  if (missingFields.length > 0) {
+    res
+      .status(400)
+      .send(`missing data for generation: ${missingFields.join(", ")}`);
+  } else {
+    next();
+  }
+};
