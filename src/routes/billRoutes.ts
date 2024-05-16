7860bbb88baf44fa2216af4d0440fd7cf7db2c82
Daadaa Nabil <103863031+RyzKum@users.noreply.github.com>
2024-05-17 00:26:06 +0200

    Added the possibility of seeing invoices (dummy for the moment using data taken from the database) and the possibility of downloading them

--- src/routes/billRoutes.ts
+++ src/routes/billRoutes.ts
@@ -1,11 +1,13 @@
 // routes/billRoutes.ts
-import { Router } from 'express';
-import { getBills, validateBill, cancelBill } from '../controllers/BillController';
+import { Router } from 'express'
+import { getBills, validateBill, cancelBill, downloadBill, viewBill } from '../controllers/BillController'
 
-const router = Router();
+const router = Router()
 
-router.get('/', getBills);
-router.put('/:id/validate', validateBill);
-router.put('/:id/cancel', cancelBill);
+router.get('/', getBills)
+router.put('/:id/validate', validateBill)
+router.put('/:id/cancel', cancelBill)
+router.get('/:id/download', downloadBill)
+router.get('/:id/view', viewBill)
 
-export default router;
+export default router
