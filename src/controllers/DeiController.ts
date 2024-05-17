cb9f03b4fcecfc93fbb3e66e1a89621d25177292
Elisa Semedo <elisasemedo07@gmail.com>
2024-05-17 15:31:57 +0200

    correction

--- src/controllers/DeiController.ts
+++ src/controllers/DeiController.ts
@@ -66,7 +66,7 @@ const updateStatusDEI = async (req: Request, res: Response) => {
               id: id,
             },
             data: {
-              status: Boolean(status),
+              status: status,
             },
           })
           
