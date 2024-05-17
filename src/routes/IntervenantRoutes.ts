4bcf762b1387641b2cff468788802f0960af43c7
Mizer3 <91741197+Mizer3@users.noreply.github.com>
2024-05-17 13:22:17 +0200

    restricted access to speaker-company

--- src/routes/IntervenantRoutes.ts
+++ src/routes/IntervenantRoutes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import {keycloak} from "index";
 
 import { getCompany } from "../controllers/IntervenantController";
 
 const router = Router();
+function authorizedUser(token: any, request: any) {
+    return token.hasRole( 'realm:buyer') || token.hasRole( 'realm:educational-assistant')|| token.hasRole( 'realm:management-controller')|| token.hasRole( 'realm:program-director')|| token.hasRole( 'realm:program-manager')
+}
 
-router.get("/", getCompany);
+router.get("/", keycloak.protect( authorizedUser ), getCompany);
 
 export default router;
