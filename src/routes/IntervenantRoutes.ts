fc6498ad50967b854f34c1012905071d8c0c5714
Mizer3 <91741197+Mizer3@users.noreply.github.com>
2024-05-18 22:15:01 +0200

    ajout acces par role dans utils

--- src/routes/IntervenantRoutes.ts
+++ src/routes/IntervenantRoutes.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import {keycloak} from "index";
-
+import authorizedUser from "../utils/accessByRole";
 import { getCompany } from "../controllers/IntervenantController";
 
 const router = Router();
-function authorizedUser(token: any, request: any) {
-    return token.hasRole( 'realm:buyer') || token.hasRole( 'realm:educational-assistant')|| token.hasRole( 'realm:management-controller')|| token.hasRole( 'realm:program-director')|| token.hasRole( 'realm:program-manager')
-}
+
 
 router.get("/", keycloak.protect( authorizedUser ), getCompany);
 
