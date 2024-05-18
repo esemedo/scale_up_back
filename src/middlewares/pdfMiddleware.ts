c9516e73c16350296a83a290d5edbf9147df4ffd
Sambachpx <113524274+Sambachpx@users.noreply.github.com>
2024-05-18 14:04:08 +0200

    fix: Remove console.log statements in pdfMiddleware.ts

--- src/middlewares/pdfMiddleware.ts
+++ src/middlewares/pdfMiddleware.ts
@@ -6,7 +6,6 @@ export const pdfMiddleware = (
   next: NextFunction
 ) => {
   const data = req.body;
-  console.log("données reçues pour la génération du PDF :", data);
 
   const missingFields = [
     "hourlyPrice",
@@ -16,12 +15,10 @@ export const pdfMiddleware = (
   ].filter((field) => !data[field]);
 
   if (missingFields.length > 0) {
-    console.log("champs manquants pour la génération du PDF :", missingFields);
     res
       .status(400)
       .send(`missing data for generation: ${missingFields.join(", ")}`);
   } else {
-    console.log("données valides pour la génération du PDF");
     next();
   }
 };
